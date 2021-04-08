1b7ce7dbb7a5f53302d93391c67e4657c8da1e82
Raunak Tamang <raunaktamang@gmail.com>
2021-04-08 12:43:49 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCRxoaMt7Qp4xakikJJNyhEJO_AA5zHupU',
-  authDomain: 'frebase-blog.firebaseapp.com',
-  projectId: 'frebase-blog',
-  storageBucket: 'frebase-blog.appspot.com',
-  messagingSenderId: '229727608667',
-  appId: '1:229727608667:web:67c0c6d61769a04ac6047e',
-  measurementId: 'G-MVR6P3T84Z',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
 };
 
 firebase.initializeApp(firebaseConfig);
